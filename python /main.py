5be6b36cfbc9e48e9ce17f2daaea5e58f6522a55
Crypt <qspoofing@gmail.com>
2023-04-14 07:24:56 -0500

    Create main.py

---
+++ python /main.py
@@ -0,0 +1,32 @@
+import requests
+
+class Domain:
+    def __init__(self, url):
+        self.url = url
+
+    def fetch(self):
+        r = requests.get(self.url)
+        return r.text.splitlines()
+
+    def blacklist(self):
+        pass
+
+class Hentai(Domain):
+    def blacklist(self):
+        blocked_sites = self.fetch()
+        with open("C:\\Windows\\System32\\drivers\\etc\\hosts", 'w+', encoding='utf-8', errors='strict') as hosts_file:
+            for site in blocked_sites:
+                if f"127.0.0.1     {site}" not in hosts_file.readlines():
+                    hosts_file.write(f"127.0.0.1     {site}\n")
+                    print(f"Blacklisted {site}")
+
+def main():
+    try:
+        hentai = Hentai("https://raw.githubusercontent.com/zEncrypte/AntiSuS/main/domains-blacklisted.txt")
+        hentai.blacklist()
+        input("Finished blacklisting sites! Press any key to close")
+    except (PermissionError):
+        exit()
+    
+if __name__ == '__main__':
+    main()
